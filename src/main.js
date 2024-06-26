1a4159d50950d89c246991682ac3d02f3b2d8b2c
AnkurRam2002 <ankurram2002@gmail.com>
2024-06-26 15:40:39 +0530

    added output messages function

--- src/main.js
+++ src/main.js
@@ -46,3 +46,25 @@ chatForm.addEventListener('submit', (e) => {
   e.target.elements.msg.value = '';
   e.target.elements.msg.focus();
 });
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').appendChild(div);
+  }
+  
+  // Add room name to DOM
+  function outputRoomName(room) {
+    roomName.innerText = room;
+  }
+  
