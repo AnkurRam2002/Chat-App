cc322714ab7c7879dd69574bf7bc50a123ffd107
AnkurRam2002 <ankurram2002@gmail.com>
2024-06-26 15:21:35 +0530

    added static pages

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = 3000 || process.env.PORT
 
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`))
