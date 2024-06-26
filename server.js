06d1f660a6271dcf1ff1ced1279771c079be316c
AnkurRam2002 <ankurram2002@gmail.com>
2024-06-26 15:25:40 +0530

    setting up socket.io

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./helpers/formatDate')
+const {
+  getActiveUser,
+  exitRoom,
+  newUser,
+  getIndividualRoomUsers
+} = require('./helpers/userHelper');
 
-const app = express()
-const PORT = 3000 || process.env.PORT
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const path = require('path')
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')))
+// this block will run when the client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = newUser(socket.id, username, room);
+
+    socket.join(user.room);
+  });
 
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`))
